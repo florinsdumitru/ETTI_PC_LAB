d0a6ca2c6b295d92fea6fb3981e5db4011dd5b46
Florin-Silviu Dumitru <florin.dumitru2610@upb.ro>
2022-10-11 15:13:45 +0300

    Lab 04 exemple platforma

---
+++ lab_04/aux_files/exemplu_bwor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 20;   // 0b0000_0000_0001_0100
+    int b = 4;    // 0b0000_0000_0000_0100
+
+    if (a | b) {  // 0b0000_0000_0001_0100
+        printf("Rezultatul este diferit de 0.\n");
+    } else {
+        printf("Rezultatul are valoarea 0.\n");
+    }
+    return 0;
+}
+
